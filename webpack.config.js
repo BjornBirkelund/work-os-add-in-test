290f56830d6a57bc63dfa853c5f9634a0ff6bafd
bjornbirkelund <bbirkelund@gmail.com>
2024-07-09 16:06:42 -0600

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,10 @@ const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const path = require("path"); // Add this line
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://test.filot.ai/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
@@ -24,6 +25,7 @@ module.exports = async (env, options) => {
       commands: "./src/commands/commands.js",
     },
     output: {
+      path: path.resolve(__dirname, "build"), // Add this line
       clean: true,
     },
     resolve: {
