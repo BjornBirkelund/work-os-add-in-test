829b28812381958a8f261ce63ea0b0dfcb704ec4
bjornbirkelund <bbirkelund@gmail.com>
2024-08-07 14:58:12 -0700

    added back workOS changes

--- src/taskpane/components/App.jsx
+++ src/taskpane/components/App.jsx
@@ -3,11 +3,10 @@ import PropTypes from "prop-types";
 import Header from "./Header";
 import HeroList from "./HeroList";
 import TextInsertion from "./TextInsertion";
-import { makeStyles } from "@fluentui/react-components";
+import { Spinner, makeStyles } from "@fluentui/react-components";
 import { Ribbon24Regular, LockOpen24Regular, DesignIdeas24Regular } from "@fluentui/react-icons";
 import { insertText } from "../taskpane";
-import { SignedIn, SignIn, SignedOut, UserButton, useUser } from "@clerk/clerk-react";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { useAuth } from "@workos-inc/authkit-react";
 
 const useStyles = makeStyles({
   root: {
@@ -16,77 +15,40 @@ const useStyles = makeStyles({
 });
 
 const App = (props) => {
-  const { title } = props;
-  const styles = useStyles();
-  const { user, isLoaded } = useUser();
-  const [loading, setLoading] = React.useState(!isLoaded);
+  const { user, getAccessToken, isLoading, signIn, signUp, signOut } = useAuth();
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  React.useEffect(() => {
-    if (isLoaded) {
-      setLoading(false);
-      // Placeholder for getExcelInfo and awaitFetchFiles functions
-      console.log("User loaded, fetching Excel info and files...");
-    }
-  }, [isLoaded]);
+  const performMutation = async () => {
+    const accessToken = await getAccessToken();
+    console.log("api request with accessToken", accessToken);
+  };
 
-  if (loading) {
+  if (user) {
     return (
-      <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-        <CircularProgress />
-      </Box>
+      <div>
+        Hello, {user.email}
+        <p>
+          <button
+            onClick={() => {
+              performMutation();
+            }}
+          >
+            Make API Request
+          </button>
+        </p>
+        <p>
+          <button onClick={() => signOut()}>Sign out</button>
+        </p>
+      </div>
     );
   }
 
-  // The list items are static and won't change at runtime,
-  // so this should be an ordinary const, not a part of state.
-  const listItems = [
-    {
-      icon: <Ribbon24Regular />,
-      primaryText: "Achieve more with Office integration",
-    },
-    {
-      icon: <LockOpen24Regular />,
-      primaryText: "Unlock features and functionality",
-    },
-    {
-      icon: <DesignIdeas24Regular />,
-      primaryText: "Create and visualize like a pro",
-    },
-  ];
-
   return (
-    <div className={styles.root}>
-      <SignedIn>
-        <Box sx={{ marginRight: "3px", marginTop: "3px", display: "flex", justifyContent: "flex-end" }}>
-          <UserButton />
-        </Box>
-        <Header logo="assets/logo-filled.png" title={title} message="Welcome" />
-        <HeroList message="Discover what this add-in can do for you today!" items={listItems} />
-        <TextInsertion insertText={insertText} />
-      </SignedIn>
-      <SignedOut>
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: 5, height: "100vh" }}>
-          <Typography sx={{ mb: 2 }} variant="h6" align="center">
-            Welcome to Filot, your AI Financial Co-Pilot
-          </Typography>
-          <Typography sx={{ mb: 2 }}>Click below to Sign In</Typography>
-          <SignIn
-            // signUpUrl="/sign-up"
-            afterSignInUrl="/taskpane.html"
-            style={{
-              cursor: "pointer",
-              padding: "8px 16px",
-              borderRadius: "4px",
-              border: "none",
-              marginBottom: "12px",
-            }}
-          >
-            Sign In
-          </SignIn>
-          <Typography variant="caption">(Refresh Add-In window to complete sign in)</Typography>
-        </Box>
-      </SignedOut>
-    </div>
+    <>
+      <button onClick={() => signIn()}>Sign in</button> <button onClick={() => signUp()}>Sign up</button>
+    </>
   );
 };
 
--- src/taskpane/index.jsx
+++ src/taskpane/index.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { AuthKitProvider } from "@workos-inc/authkit-react";
 
 /* global document, Office, module, require */
 
@@ -13,26 +13,18 @@ const root = rootElement ? createRoot(rootElement) : undefined;
 
 /* Render application after Office initializes */
 Office.onReady(() => {
-  const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuZmlsb3QuYWkk";
-  const FRONTEND_URL = "https://test.filot.ai";
   root?.render(
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignInUrl={FRONTEND_URL}>
+    <AuthKitProvider clientId="client_01J4Q0J2F830SXK60KBGG9WP9Z">
       <FluentProvider theme={webLightTheme}>
         <App title={title} />
       </FluentProvider>
-    </ClerkProvider>
+    </AuthKitProvider>
   );
 });
 
 if (module.hot) {
   module.hot.accept("./components/App", () => {
     const NextApp = require("./components/App").default;
-    root?.render(
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignInUrl={FRONTEND_URL}>
-        <FluentProvider theme={webLightTheme}>
-          <NextApp title={title} />
-        </FluentProvider>
-      </ClerkProvider>
-    );
+    root?.render(NextApp);
   });
 }
